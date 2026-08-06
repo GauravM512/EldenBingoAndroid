c8db5ef7227e7979ea1687a2e13bd8e1bec89332
GauravM512 <gauravm5612@gmail.com>
2026-08-06 10:30:18 +0530

    chore: Bump app version to 1.1.0 for Server 0.18 compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 
 object AppVersion {
     const val MAJOR = 1
-    const val MINOR = 0
-    const val PATCH = 1
+    const val MINOR = 1
+    const val PATCH = 0
     const val BUILD = 1
 
     const val VERSION_NAME = "$MAJOR.$MINOR.$PATCH"
